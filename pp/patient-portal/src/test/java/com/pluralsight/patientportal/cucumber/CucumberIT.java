fac9bbb8f6f4936fdad34de7d9eb491febb07e93
Leulsegedgit <64631411+Leulsegedgit@users.noreply.github.com>
2023-05-06 11:37:55 +0300

    Initial version of PatientPortal generated by generator-jhipster@7.9.3

---
+++ pp/patient-portal/src/test/java/com/pluralsight/patientportal/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package com.pluralsight.patientportal.cucumber;
+
+import com.pluralsight.patientportal.IntegrationTest;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
---
+++ pp/patient-portal/src/test/java/com/pluralsight/patientportal/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,8 @@
+package com.pluralsight.patientportal.cucumber.stepdefs;
+
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+public abstract class StepDefs {
+
+    protected WebTestClient.ResponseSpec actions;
+}
